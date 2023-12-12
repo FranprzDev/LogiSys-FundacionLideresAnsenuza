41e0484fab933089654bb1267f6d5162e97f575c
FranprzDev <franutn23@gmail.com>
2023-12-12 20:29:58 -0300

    finalizacion proyecto logisys

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import Header from './Header'
-import Home from './Home'
-import Nosotros from './Nosotros'
-import PrimeraEtapa from './Etapas/PrimeraEtapa'
-import SegundaEtapa from './Etapas/SegundaEtapa'
-import TerceraEtapa from './Etapas/TerceraEtapa'
-import CuartaEtapa from './Etapas/CuartaEtapa'
-import Final from './Final'
+import Header from "./Header";
+import Home from "./Home";
+import Nosotros from "./Nosotros";
+import PrimeraEtapa from "./Etapas/PrimeraEtapa";
+import SegundaEtapa from "./Etapas/SegundaEtapa";
+import TerceraEtapa from "./Etapas/TerceraEtapa";
+import CuartaEtapa from "./Etapas/CuartaEtapa";
+import Final from "./Final";
+import LinkPage from "./Etapas/LinkPage";
 
 function App() {
-
   return (
-    <> 
+    <>
       <Header />
-      <section className='w-100 h-100'>
-        <Home/>
+      <section className="w-100 h-100">
+        <Home />
         <Nosotros />
 
-        <section className='py-5'
-        >
+        <section className="py-5">
           <PrimeraEtapa />
           <SegundaEtapa />
           <TerceraEtapa />
           <CuartaEtapa />
+          <section className="container">
+            <LinkPage 
+              title="TFI COMPLETO" 
+              link="https://drive.google.com/file/d/1eUB-BdWEdfxao_OEyxKy5RZr5bTcItS6/view?usp=drive_link"         
+              index={1} />
+          </section>
         </section>
 
-        <Final/>
+        <Final />
       </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Final.jsx
+++ src/Final.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import '../src/index.css'
 
 function Final() {
   return (
@@ -8,12 +9,12 @@ function Final() {
           <div className="col-12 col-md-6">
             <img src="./racoon.png" alt="Racoon" className="img-fluid" />
           </div>
-          <div className="col-12 col-md-6 d-flex justify-content-center align-items-center">
-            <h2 className="text-wrap text-Primary-Dark fs-4 fw-bold text-center">
-              Definitivamente, el proceso de análisis del sistema previo a su desarrollo nos brinda
-              una visión sumamente clara y detallada del proyecto, como si fuese un rompecabezas, 
-              entendiendo cada pieza y cómo encajan entre sí para formar el panorama completo;
-              que será el próximo sistema a desarollar.
+          <div className="col-12 col-md-6 d-flex justify-content-center align-items-center ">
+            <h2 className="text-wrap text-Primary-Dark fs-4 fw-bold text-center" id="animate-bounce">
+              La finalización de esta aventura nos ha mostrado que nuestro
+              aprendizaje fue más allá de la adquisición de conocimiento
+              teórico; fuimos capaces de traducirlo y aplicarlo en soluciones
+              reales y beneficiosas para la organización estudiada.
             </h2>
           </div>
         </div>
